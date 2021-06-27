a5ae7413bebb388c671788eacb5b6c3503fbe8d2
Puszkarek <52247963+Puszkarek@users.noreply.github.com>
2021-06-27 14:53:06 -0300

    reorganize function

--- msg-handler.js
+++ msg-handler.js
@@ -7,8 +7,8 @@ global.DATA_FNS = {
 	format,
 	formatDistance,
 	formatRelative,
-	subDays
-} = require("date-fns")
+	subDays,
+} = require("date-fns");
 const chalk = require("chalk");
 const fetch = require("node-fetch");
 //* global functions
@@ -18,7 +18,7 @@ global.FUNCTIONS = {
 	getFilterPath,
 	configURLParams,
 	requestFrom,
-	capitalizeText
+	capitalizeText,
 } = require("./lib/functions/global-functions.js");
 //settings
 global.OPTIONS = {
@@ -31,17 +31,16 @@ global.OPTIONS = {
 	botNumber,
 } = require("./lib/options.js");
 //import
-const functions =
-	Object.assign(
-		require('./lib/functions/help-handler'),
-		require("./lib/functions/entertainment-handler"),
-		require("./lib/functions/math-handler"),
-		require("./lib/functions/util-handler"),
-		require("./lib/functions/api-handler"),
-		require("./lib/cards-against-humanity/game"),
-		require("./lib/functions/filter-handler"))
-
-const iaHandler = require("./lib/functions/ia-handler")
+const functions = Object.assign(
+	require("./lib/functions/help-handler"),
+	require("./lib/functions/entertainment-handler"),
+	require("./lib/functions/math-handler"),
+	require("./lib/functions/util-handler"),
+	require("./lib/functions/api-handler"),
+	require("./lib/cards-against-humanity/game"),
+	require("./lib/functions/filter-handler"),
+	require(`./lib/language/${language}/commands.js`),
+);
 const libPath = `./lib/language/${language}`;
 //lang import
 global.RESPONSES = require(`${libPath}/RESPONSES.js`);
@@ -49,24 +48,35 @@ global.RESPONSES["help"] = JSON.parse(fs.readFileSync(`${libPath}/help.json`));
 global.INSULTS = require(`${libPath}/insults.json`);
 
 //IA
-const botAI = require("cleverbot-free");
-const m = require("gm");
+const iaHandler = require("./lib/functions/ai-handler");
+const stickerPackName = RESPONSES.stickerPackName;
+const stickerPackAuthor = RESPONSES.stickerPackAuthor;
 
 module.exports = async (client, message) => {
 	try {
 		const callback = async function (body) {
 			const responseTypes = {
 				async reply() {
-					await client.reply(from, body.response, id)
+					await client.reply(from, body.response, id);
 				},
 				async animatedSticker() {
-					await client.sendMp4AsSticker(from, body.media);
+					await client.sendMp4AsSticker(
+						from,
+						body.media,
+						{ crop: false },
+						{ pack: stickerPackName, author: stickerPackAuthor },
+					);
 				},
 				async imageWithCaption() {
-					await client.sendImage(from, body.imageURI, body.filetype, body.caption)
+					await client.sendImage(
+						from,
+						body.imageURI,
+						body.filetype,
+						body.caption,
+					);
 				},
 				async image() {
-					await client.sendImage(from, body.imageURI, body.filetype)
+					await client.sendImage(from, body.imageURI, body.filetype);
 				},
 				async audio() {
 					client.sendAudio(from, body.link, id);
@@ -76,8 +86,13 @@ module.exports = async (client, message) => {
 					const contactIdList = body.contactId;
 					const contactLength = contactIdList.length;
 					for (let i = 0; i < contactLength; i++) {
-						const contact = await client.getContact(contactIdList[i]);
-						const contactName = contact.pushname != null ? contact.pushname : contact.formattedName
+						const contact = await client.getContact(
+							contactIdList[i],
+						);
+						const contactName =
+							contact.pushname != null
+								? contact.pushname
+								: contact.formattedName;
 						coupleList += contactName;
 						if (i != contactLength - 1) {
 							coupleList += " e ";
@@ -90,71 +105,74 @@ module.exports = async (client, message) => {
 					await client.sendTextWithMentions(from, body.response);
 				},
 				async sticker() {
-					await client.sendImageAsSticker(from, body.imageURI);
+					await client.sendImageAsSticker(from, body.imageURI, {
+						pack: stickerPackName,
+						author: stickerPackAuthor,
+					});
 				},
 				async link() {
-					await client.sendLinkWithAutoPreview(from, body.link, body.caption);
+					await client.sendLinkWithAutoPreview(
+						from,
+						body.link,
+						body.caption,
+					);
 				},
 				async message() {
 					await client.sendText(body.chatId, body.response);
 				},
 				async fileFromUrlWithImage() {
-					await client.sendFileFromUrl(from, body.image, body.fileType, body.url, id)
+					await client.sendFileFromUrl(
+						from,
+						body.image,
+						body.fileType,
+						body.url,
+						id,
+					);
 				},
 				async error() {
-					console.log(body.errorType)
-					if (from.id == ownerNumber) await client.reply(from, body.errorType.toString(), id)
-					else await client.reply(from, body.response, id)
-
-				}
-			}
+					console.log(body.errorType);
+					if (from.id == ownerNumber)
+						await client.reply(from, body.errorType.toString(), id);
+					else await client.reply(from, body.response, id);
+				},
+			};
 			const clientResponse = responseTypes[body.type];
-			if (typeof clientResponse != 'function') return;
+			if (typeof clientResponse != "function") return;
 			clientResponse();
-		}
-		const {
-			id,
-			from,
-			sender,
-			isGroupMsg,
-			chat,
-			caption,
-			quotedMsg
-		} = message;
-		const {
-			formattedTitle,
-			groupMetadata
-		} = chat;
+		};
+		const { id, from, sender, isGroupMsg, chat, caption, quotedMsg } =
+			message;
+		const { formattedTitle, groupMetadata } = chat;
 		//commands
 		let argumentsTxt = caption || message.body || "";
 		argumentsTxt = argumentsTxt.toLowerCase();
-		const argumentsList = argumentsTxt.split(" ");
 		let quoteTxt = "";
-		if (quotedMsg != null) quoteTxt = quotedMsg.caption || (quotedMsg.type == 'chat' ? quotedMsg.body : '');
+		if (quotedMsg != null)
+			quoteTxt =
+				quotedMsg.caption ||
+				(quotedMsg.type == "chat" ? quotedMsg.body : "");
 
 		const groupId = isGroupMsg ? groupMetadata.id : "";
 		// * verify if is a function
-		if (message.author == ownerNumber && argumentsTxt.includes("test")) {
-
-			console.log(message)
-		}
-		if (argumentsTxt.startsWith(prefix)) {
+		if (argumentsTxt.startsWith(prefix) && argumentsTxt.length > 1) {
 			//formatted log
+			argumentsTxt = argumentsTxt.substr(1).trim();
 			console.log(
 				chalk.green(sender.pushname + ":"),
 				chalk.yellow('"' + argumentsTxt + '"'),
 				chalk.green(" at"),
 				chalk.magenta(format(new Date(), "HH:mm")),
-				isGroupMsg ? chalk.green("from ") +
-				chalk.red(formattedTitle) : "");
+				isGroupMsg
+					? chalk.green("from ") + chalk.red(formattedTitle)
+					: "",
+			);
+
+			const argumentsList = argumentsTxt.split(" ");
 			let command = argumentsList.shift() || "";
-			let commandWordIndex = argumentsTxt.indexOf(" ") + 1;
-			argumentsTxt = !(commandWordIndex == 0) ? argumentsTxt.substr(commandWordIndex) : "";
-			command = command.substr(1);
+			argumentsTxt = argumentsList.join(" ");
 			//* get function
 			let functionToExecute = functions[command];
 			if (functionToExecute == null) return;
-			// ! temporary solution
 			functionToExecute(callback, {
 				argumentsTxt,
 				argumentsList,
@@ -162,18 +180,25 @@ module.exports = async (client, message) => {
 				chat,
 				isGroupMsg,
 				groupId,
-				message
+				message,
 			});
 		} else {
-			let quoteMessageSender = ''
-			if (quotedMsg != null) quoteMessageSender = quotedMsg.from
-			const replyIa = await iaHandler(callback, argumentsTxt, quoteTxt, quoteMessageSender)
+			let quoteMessageSender = "";
+			if (quotedMsg != null) quoteMessageSender = quotedMsg.from;
+
+			const replyIa = await iaHandler(
+				callback,
+				argumentsTxt,
+				quoteTxt,
+				quoteMessageSender,
+				groupId,
+			);
 			if (replyIa == false) {
 				if (!isGroupMsg) return;
 				functions.searchFilter(callback, {
 					argumentsTxt,
-					groupId
-				})
+					groupId,
+				});
 			}
 		}
 	} catch (error) {
